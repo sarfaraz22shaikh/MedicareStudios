c81b703c3544836c1d5138d3c3af8d670512ee7c
Prasad-555 <prasadchinchole555@gmail.com>
2024-10-02 00:07:58 +0530

    Trial version 0.10

---
+++ app/src/main/java/com/developer/opdmanager/doctor_info_card.java
@@ -0,0 +1,12 @@
+package com.developer.opdmanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class doctor_info_card extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_info_card);
+    }
+}
