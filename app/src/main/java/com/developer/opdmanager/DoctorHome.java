d0869dc1241a2e612c8e0ac4954558cafd37c0f3
sarfaraz shaikh <143616010+sarfaraz22shaikh@users.noreply.github.com>
2025-03-26 00:05:40 +0530

    Create Slot activity added

---
+++ app/src/main/java/com/developer/opdmanager/DoctorHome.java
@@ -0,0 +1,80 @@
+package com.developer.opdmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DoctorHome#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DoctorHome extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DoctorHome() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DoctorHome.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DoctorHome newInstance(String param1, String param2) {
+        DoctorHome fragment = new DoctorHome();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private Button CreateSlot;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_doctor_home, container, false);
+        CreateSlot = view.findViewById(R.id.create_slot_button);
+        CreateSlot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("pogo","pankaj");
+                Intent intent = new Intent(getActivity(), CreateSlots.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
